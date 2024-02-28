227e12f3a93d7c4304f402599e704df953b92b8b
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-27 21:36:35 -0500

    Adding comments to main.py file

--- main.py
+++ main.py
@@ -10,19 +10,24 @@ option = st.selectbox("Select data to view forecast for", ("Temperature", "Sky")
 st.subheader(f"{option} forecast for the next {days} days in {place}")
 
 
+# If a location is provided
 if place:
-    # Get the temperature/sky data
+    # Get the temperature/sky data for the specified location and forecast days
     filtered_data = get_data(place, days)
 
+    # If the selected option is "Temperature"
     if option == "Temperature":
+        # Extract temperature data in Kelvin from the filtered data
         temperatures_k = [dict["main"]["temp"] for dict in filtered_data]
         # Convert the degree from Kelvin to Celsius
         temperatures_c = [temp - 273.15 for temp in temperatures_k]
+        # Extract dates from the filtered data
         dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
+        # Create a line plot for temperature forecast
         figure = px.line(x=dates, y=temperatures_c, labels={"x": "Date", "y": "Temperature (C)"})
         st.plotly_chart(figure)
 
+    # If the selected option is "Sky"
     if option == "Sky":
         images = {"Clear": "images/clear.png", "Snow": "images/snow.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png"}
         sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
