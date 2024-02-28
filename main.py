16def49e487d0a6c65f6875baa8308f2246c5805
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-27 20:28:49 -0500

    Adding Sky Condition Images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
+from backed import get_data
 
+# Add title, text input, slider, select box, and subheader elements
 st.title("🌤️🌦️☀️")
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -8,13 +10,9 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view forecast for", ("Temperature", "Sky"))
 st.subheader(f"{option} forecast for the next {days} days in {place}")
 
+# Get the temperature/sky data
+d, t = get_data(place, days, option)
 
-def get_date(days):
-    dates = ["2022-25-10", "2022-25-11", "2022-25-12"]
-    temperatures = [10,11,15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-d, t = get_date(days)
+# Create a temperature plot
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
