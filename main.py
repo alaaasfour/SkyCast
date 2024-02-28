974dbd8f58bcbd5062fdc42e39c103c08ae277cb
Alaa Asfour <alaa.asfour94@outlook.com>
2024-02-27 21:45:37 -0500

    Fix Place Bug
    Write an error message if the user enters an unknown city/place

--- main.py
+++ main.py
@@ -13,23 +13,26 @@ st.subheader(f"{option} forecast for the next {days} days in {place}")
 # If a location is provided
 if place:
     # Get the temperature/sky data for the specified location and forecast days
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
-    # If the selected option is "Temperature"
-    if option == "Temperature":
-        # Extract temperature data in Kelvin from the filtered data
-        temperatures_k = [dict["main"]["temp"] for dict in filtered_data]
-        # Convert the degree from Kelvin to Celsius
-        temperatures_c = [temp - 273.15 for temp in temperatures_k]
-        # Extract dates from the filtered data
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a line plot for temperature forecast
-        figure = px.line(x=dates, y=temperatures_c, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        # If the selected option is "Temperature"
+        if option == "Temperature":
+            # Extract temperature data in Kelvin from the filtered data
+            temperatures_k = [dict["main"]["temp"] for dict in filtered_data]
+            # Convert the degree from Kelvin to Celsius
+            temperatures_c = [temp - 273.15 for temp in temperatures_k]
+            # Extract dates from the filtered data
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a line plot for temperature forecast
+            figure = px.line(x=dates, y=temperatures_c, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    # If the selected option is "Sky"
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Snow": "images/snow.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        # If the selected option is "Sky"
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Snow": "images/snow.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.error("That place doesn't exist!")
